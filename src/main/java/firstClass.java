33e45bfc28b59a7aecb1af59bff2fc48b28bc0c3
onas2729 <karabacakn35@gmail.com>
2019-11-21 18:35:34 -0500

    new new

--- src/main/java/firstClass.java
+++ src/main/java/firstClass.java
@@ -3,7 +3,7 @@ public class firstClass {
     public static void main(String[] args) {
         System.out.println("OSman");
 
-                System.out.println("Tarik");
+                System.out.println("Osman");
 
     
     }
