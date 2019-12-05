f26605ff8f87d7263fe15884a3e1f115cfe6870e
onas2729 <54293667+onas2729@users.noreply.github.com>
2019-12-05 16:05:28 -0500

    some changes

--- src/main/java/SecondClass.java
+++ src/main/java/SecondClass.java
@@ -1,6 +1,7 @@
 public class SecondClass {
     public static void main(String[] args) {
-       
+       String myname="John B Goodenough";
+        System.out.println(myname);
         System.out.println("Ali Karabacak");
         System.out.println("Omer Sami Karabacak");
          System.out.println("Nese Karabacak");
