f9ba28a849d63a98929650fec09d3e9de9552eb6
onas2729 <karabacakn35@gmail.com>
2019-12-05 17:06:45 -0500

    'aaa'

--- src/main/java/ThirdClass.java
+++ src/main/java/ThirdClass.java
@@ -1,6 +1,6 @@
 public class ThirdClass {
     public static void main(String[] args) {
-        System.out.println("Osman");
+        System.out.println("Nese");
         System.out.println("Omer Sami");
 
     }
