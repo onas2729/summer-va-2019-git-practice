c0d4a2b2f6d2b916c9021b95937a0af16f79bb5f
onas2729 <karabacakn35@gmail.com>
2019-12-12 11:59:06 -0500

    'some'

--- src/main/java/SecondClass.java
+++ src/main/java/SecondClass.java
@@ -1,7 +1,6 @@
 public class SecondClass {
     public static void main(String[] args) {
-        String myname="John Goodenough";
-        System.out.println(myname);
+
         System.out.println("Ali Karabacak");
         System.out.println("Omer Sami Karabacak");
         System.out.println("Nese Karabacak");
